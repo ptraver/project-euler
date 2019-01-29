65c14d158a92c2638a8809a3f84fc2ffba323c42
ptraver <ptraver@tcd.ie>
2019-01-29 13:21:27 +0000

    complete project euler problem 3

---
+++ euler3.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 11:30:37 2019
+
+@author: 33743
+
+"""
+
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+from math import sqrt, floor
+
+large_num = 600851475143
+
+#Identifies all the factors of large_num
+factors = [a for a in range(1, floor(sqrt(large_num))) if large_num % a == 0]
+
+#makes a function which establishes whether prime or not
+def is_prime(num):
+    if num == 0: return None
+    if num == 1: return True
+    
+    for a in range(2, floor(sqrt(num))):
+        if num % a == 0:
+            return False
+        
+    return True
+
+print(max(a for a in factors if is_prime(a) == True))
